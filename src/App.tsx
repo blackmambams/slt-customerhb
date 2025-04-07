17d89d07e77f3447c906812e74bc9508954f27a1
milanshahama <milanshahama@gmail.com>
2025-04-07 20:43:05 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { SearchScreen } from './components/SearchScreen';
+import { CustomerDetails } from './pages/CustomerDetails';
+import { AdminPage } from './pages/AdminPage';
+import { BackgroundAnimation } from './components/BackgroundAnimation';
+import { LanguageProvider } from './components/LanguageContext';
+import { LanguageSwitcher } from './components/LanguageSwitcher';
+export function App() {
+  return <LanguageProvider>
+      <BrowserRouter>
+        <div className="flex flex-col w-full min-h-screen bg-gray-900 text-white relative overflow-hidden">
+          <BackgroundAnimation />
+          <LanguageSwitcher />
+          <Routes>
+            <Route path="/" element={<SearchScreen />} />
+            <Route path="/customer/:id" element={<CustomerDetails />} />
+            <Route path="/admin" element={<AdminPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </LanguageProvider>;
+}
